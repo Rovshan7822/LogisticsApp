1abb90400b09e4d27e2c41ab310d8f524efd1967
Ravshan Tuymurodov <ravshantuymurodov39@gmail.com>
2025-02-23 20:12:35 +0500

    first commit

--- src/main/java/com/ravshan/logistics_app/LogisticsAppApplication.java
+++ src/main/java/com/ravshan/logistics_app/LogisticsAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LogisticsAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LogisticsAppApplication.class, args);
+		SpringApplication.run(LogisticsAppApplication.class, args); 
 	}
 
 }
